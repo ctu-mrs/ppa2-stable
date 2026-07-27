9523b85ac01fd783c389b2a34f24b2852db460cb
Filip <4060824+filipopo@users.noreply.github.com>
2026-07-27 13:16:30 +0200

    Add space

--- resources/scripts/add_sources_ppa.sh
+++ resources/scripts/add_sources_ppa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors "https://ctu-mrs.github.io/ppa2-stable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg >/dev/null
+curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors "https://ctu-mrs.github.io/ppa2-stable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg > /dev/null
 sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors -o /etc/apt/sources.list.d/ctu-mrs-stable.list "https://ctu-mrs.github.io/ppa2-stable/ctu-mrs-apt.list"
 sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-all-errors -o /etc/apt/preferences.d/ctu-mrs-stable-preferences "https://ctu-mrs.github.io/ppa2-stable/ctu-mrs-ppa-preferences.txt"
 
