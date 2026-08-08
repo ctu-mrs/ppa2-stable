c051a3af40fd34c7eab001615e75c5ac92c4e6d6
cturmrsbot <cturmrsbot@users.noreply.github.com>
2026-08-08 16:34:02 +0000

    deploy: 733d079690b0f1024397b3dbf44194f7981c42e7

---
+++ remove_ppa.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "$0: Removing MRS ROS2 Stable PPA repository"
+
+sudo rm /etc/apt/sources.list.d/ctu-mrs-stable.list
+sudo rm /etc/apt/preferences.d/ctu-mrs-stable-preferences
+sudo rm /etc/ros/rosdep/sources.list.d/ctu-mrs-stable.list
+
+if [ -n "$ROS_DISTRO" ]; then
+  DISTRO="--rosdistro=$ROS_DISTRO"
+fi
+
+sudo apt-get -o Acquire::Retries=4 update
+rosdep $DISTRO update
+
+echo "$0: Finished removing MRS ROS2 Stable PPA repository"
